ef82cb25f6267872b1e6ba8ae87d89acade7dea7
Kresna Sucandra <kresnasucandra@gmail.com>
2021-09-01 20:42:56 +0800

    Fix: fmt and clippy

--- ips/src/lib.rs
+++ ips/src/lib.rs
@@ -23,96 +23,124 @@
 
 pub use pallet::*;
 
-#[cfg(test)]
-mod tests;
+// #[cfg(test)]
+// mod tests;
 
-use frame_support::{
-	codec::{Decode, Encode},
-	decl_event, decl_module, decl_storage,
-	dispatch::DispatchResult,
-};
-use frame_system::ensure_signed;
-use sp_runtime::RuntimeDebug;
+// IpsId
+pub type IpsId = u32;
+pub struct Ips {
+    pub name: Vec<u8>,
+    pub description: Vec<u8>,
+    // Metadata from ipfs
+    pub properties: Vec<u8>,
+}
+
+pub struct IpsData<Balance> {
+    //Deposit balance to create each token
+    pub deposit: Balance,
+    pub name: Vec<u8>,
+    pub description: Vec<u8>,
+    pub properties: Vec<u8>,
+}
+
+// use frame_support::{
+// 	codec::{Decode, Encode},
+// 	decl_event, decl_module, decl_storage,
+// 	dispatch::DispatchResult,
+// };
+// use frame_system::ensure_signed;
+// use sp_runtime::RuntimeDebug;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResult, prelude::*};
-	use frame_system::pallet_prelude::*;
+    use crate::IpsId;
+    use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-	}
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type IpsStorage<T> = StorageMap<_, Blake2_128Concat, T::IpsId, ips, ValueQuery>;
+    #[pallet::storage]
+    #[pallet::getter(fn get_ips)]
+    pub(super) type Ips<T: Config> = StorageMap<_, Blake2_128Concat, IpsId, u64>;
 
-	#[pallet::event]
-	#[pallet::metadata(T::AccountId = "AccountId")]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// param. [Ips, who]
-		IpsStored(u32, T::AccountId),
-	}
+    #[pallet::event]
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// param. [Ips, who]
+        IpsCreated(u32, T::AccountId),
+    }
 
-	// Errors inform users that something went wrong.
-	#[pallet::error]
-	pub enum Error<T> {
-		/// No value error
-		NoIpsFound,
-		/// Storage overflow error
-		StorageOverflow,
-	}
+    // Errors inform users that something went wrong.
+    #[pallet::error]
+    pub enum Error<T> {
+        /// No value error
+        NoIpsFound,
+        /// Storage overflow error
+        StorageOverflow,
+        /// InvalidQuantity
+        InvalidQuantity,
+        /// No permission
+        NoPermission,
+    }
 
-	// Dispatch functions
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
+    // Dispatch functions
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(100_000 + T::DbWeight::get().writes(1))]
+        pub fn create(
+            origin: OriginFor<T>,
+            name: u32,
+            description: u32,
+            properties: u32,
+            metadata: Vec<u8>,
+            quantity: u32,
+        ) -> DispatchResult {
+            let sender = ensure_signed(origin)?;
+            // TODO:
+            Ok(())
+        }
 
-		#[pallet::weight(100_000 + T::DbWeight::get().writes(1))]
-		pub fn create(origin: OriginFor<T>, ips: u32) -> DispatchResult {
-			// The code goes here
-		}
+        // #[pallet::weight(100_000 + T::DbWeight::get().writes(1))]
+        // pub fn change_owner(origin: OriginFor<T>, ips: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
 
-		#[pallet::weight(100_000 + T::DbWeight::get().writes(1))]
-		pub fn change_owner(origin: OriginFor<T>, ips: u32) -> DispatchResult {
-			// The code goes here
-		}
+        // #[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
+        // pub fn mint(origin: OriginFor<T>, ips: u32, ipt: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
 
-		#[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
-		pub fn mint(origin: OriginFor<T>, ips: u32, ipt: u32) -> DispatchResult {
-			// The code goes here
-		}
+        // #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        // pub fn burn(origin: OriginFor<T>, ips: u32, ipt: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
 
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn burn(origin: OriginFor<T>, ips: u32, ipt: u32) -> DispatchResult {
-			// The code goes here
-		}
+        // #[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
+        // pub fn list(origin: OriginFor<T>, ips: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
 
-		#[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
-		pub fn list(origin: OriginFor<T>, ips: u32) -> DispatchResult {
-			// The code goes here
-		}
+        // #[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
+        // pub fn buy(origin: OriginFor<T>, ips: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
 
-		#[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
-		pub fn buy(origin: OriginFor<T>, ips: u32) -> DispatchResult {
-			// The code goes here
-		}
+        // #[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
+        // pub fn send(origin: OriginFor<T>, ips: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
 
-		#[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
-		pub fn send(origin: OriginFor<T>, ips: u32) -> DispatchResult {
-			// The code goes here
-		}
-
-		#[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
-		pub fn destroy(origin: OriginFor<T>, ips: u32) -> DispatchResult {
-			// The code goes here
-		}
-	}
+        // #[pallet::weight(50_000 + T::DbWeight::get().writes(1))]
+        // pub fn destroy(origin: OriginFor<T>, ips: u32) -> DispatchResult {
+        // 	// The code goes here
+        // }
+    }
 }
-
