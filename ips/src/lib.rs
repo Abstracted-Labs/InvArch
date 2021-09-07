f247d6c38ccee977eec426f987cf75f25861ba90
Kresna Sucandra <kresnasucandra@gmail.com>
2021-09-07 21:53:01 +0800

    Feat: add change-owner fn in ips pallet

--- ips/src/lib.rs
+++ ips/src/lib.rs
@@ -20,98 +20,185 @@
 //! - `destroy` - Delete an IP Set and all of its contents
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::unused_unit)]
 
-/*
-// Import IPT Pallet
-// use ipt::Pallet as IPT;
-*/
+use frame_support::{pallet_prelude::*, traits::Get, BoundedVec, Parameter};
+use sp_runtime::{
+    traits::{
+        AtLeast32BitUnsigned, CheckedAdd, MaybeSerializeDeserialize, Member, One,
+    },
+    DispatchError,
+};
+use sp_std::{convert::TryInto, vec::Vec};
 
-/// IpsId
-pub type IpsId = u32;
+// #[cfg(test)]
+// mod mock;
+// #[cfg(test)]
+// mod tests;
 
-/// IP Set struct
+/// Import from IPT pallet
+use ipt::{IpsInfo, IptInfo};
 
-pub struct Ips<Balance> {
-    pub name: Vec<u8>,
-    pub data: IpsData<Balance>,
-    pub description: Vec<u8>,
-    /// Metadata from ipfs
-    pub metadata: Vec<u8>,
-}
-
-#[derive(Clone, Default, PartialEq, Eq)]
-pub struct IpsData<Balance> {
-    /// Deposit balance to create each token
-    pub deposit: Balance,
-    pub name: Vec<u8>,
-    pub description: Vec<u8>,
-    pub metadata: Vec<u8>,
-}
+pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use crate::IpsId;
-    use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
-    use frame_system::pallet_prelude::*;
-
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    pub struct Pallet<T>(_);
+    use super::*;
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        /// The IPS ID type
+        type IpsId: Parameter + Member + AtLeast32BitUnsigned + Default + Copy; // TODO: WIP
+        /// The IPS properties type
+        type IpsData: Parameter + Member + MaybeSerializeDeserialize; // TODO: WIP
+        /// The IPT ID type
+        type IptId: Parameter + Member + AtLeast32BitUnsigned + Default + Copy;
+        /// IPT properties type
+        type IptData: Parameter + Member + MaybeSerializeDeserialize; // TODO: WIP
+        /// The maximum size of an IPS's metadata
+        type MaxIpsMetadata: Get<u32>; // TODO: WIP
+        /// The maximum size of an IPT's metadata
+        type MaxIptMetadata: Get<u32>;
     }
 
-    pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+    pub type IpsMetadataOf<T> = BoundedVec<u8, <T as Config>::MaxIpsMetadata>;
+    pub type IptMetadataOf<T> = BoundedVec<u8, <T as Config>::MaxIptMetadata>;
+    pub type IpsInfoOf<T> = IpsInfo<
+        <T as Config>::IptId,
+        <T as frame_system::Config>::AccountId,
+        <T as Config>::IpsData,
+        IpsMetadataOf<T>,
+    >;
+    pub type IptInfoOf<T> =
+        IptInfo<<T as frame_system::Config>::AccountId, <T as Config>::IptData, IptMetadataOf<T>>;
 
+    pub type GenesisIptData<T> = (
+        <T as frame_system::Config>::AccountId, // IPT owner
+        Vec<u8>,                                // IPT metadata
+        <T as Config>::IptData,                 // IPT data
+    );
+    pub type GenesisIps<T> = (
+        <T as frame_system::Config>::AccountId, // IPS owner
+        Vec<u8>,                                // IPS metadata
+        <T as Config>::IpsData,                 // IPS data
+        Vec<GenesisIptData<T>>,                 // Vector of IPTs belong to this IPS
+    );
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    /// Next available IPS ID.
     #[pallet::storage]
-    #[pallet::getter(fn get_ips)]
-    pub(super) type Ips<T: Config> = StorageMap<_, Blake2_128Concat, IpsId, u64>;
+    #[pallet::getter(fn next_ips_id)]
+    pub type NextIpsId<T: Config> = StorageValue<_, T::IpsId, ValueQuery>;
 
-    #[pallet::event]
-    #[pallet::metadata(T::AccountId = "AccountId")]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    pub enum Event<T: Config> {
-        /// param. [Ips, who]
-        IpsCreated(AccountIdOf<T>, u32, u32, Vec<u8>, u32),
-    }
+    /// Next available IPT ID
+    #[pallet::storage]
+    #[pallet::getter(fn next_ipt_id)]
+    pub type NextIptId<T: Config> = StorageMap<_, Blake2_128Concat, T::IpsId, T::IptId, ValueQuery>;
 
+    /// Store IPS info
+    ///
+    /// Return `None` if IPS info not set of removed
+    #[pallet::storage]
+    #[pallet::getter(fn ips_storage)]
+    pub type IpsStorage<T: Config> = StorageMap<_, Blake2_128Concat, T::IpsId, IpsInfoOf<T>>;
+
+    /// Store IPT info
+    ///
+    /// Returns `None` if IPT info not set of removed
+    #[pallet::storage]
+    #[pallet::getter(fn ipt_storage)]
+    pub type IptStorage<T: Config> =
+        StorageDoubleMap<_, Blake2_128Concat, T::IpsId, Blake2_128Concat, T::IptId, IptInfoOf<T>>;
+
+    /// IPT existence check by owner and IPS ID
+    #[pallet::storage]
+    #[pallet::getter(fn ipt_by_owner)]
+    pub type IptByOwner<T: Config> = StorageNMap<
+        _,
+        (
+            NMapKey<Blake2_128Concat, T::AccountId>, // owner
+            NMapKey<Blake2_128Concat, T::IpsId>,
+            NMapKey<Blake2_128Concat, T::IptId>,
+        ),
+        (),
+        ValueQuery,
+    >;
+
+    /// Errors for IPT pallet
     #[pallet::error]
     pub enum Error<T> {
-        /// No value error
-        NoIpsFound,
-        /// InvalidQuantity
-        InvalidQuantity,
-        /// No permission
+        /// No available IPS ID
+        NoAvailableIpsId,
+        /// No available IPT ID
+        NoAvailableIptId,
+        /// IPT (IpsId, IptId) not found
+        IptNotFound,
+        /// IPS not found
+        IpsNotFound,
+        /// The operator is not the owner of the IPT and has no permission
         NoPermission,
+        /// Failed because the Maximum amount of metadata was exceeded
+        MaxMetadataExceeded,
     }
 
+    /// Dispatch functions
     #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        #[pallet::weight(100_000 + T::DbWeight::get().writes(1))]
-        pub fn create(
-            owner_id: OriginFor<T>,
-            name: u32,
-            description: u32,
-            metadata: Vec<u8>,
-            quantity: u32,
-        ) -> DispatchResult {
-            let sender = ensure_signed(owner_id)?;
+    impl<T: Config> Pallet<T> {}
 
-            // TODO : WIP
+    #[pallet::hooks]
+    impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+}
 
-            ensure!(quantity >= 1, Error::<T>::InvalidQuantity);
+impl<T: Config> Pallet<T> {
+    /// Create IP (Intellectual Property) Set (IPS)
+    pub fn create_ips(
+        // TODO: WIP
+        owner: &T::AccountId,
+        metadata: Vec<u8>,
+        data: T::IpsData,
+    ) -> Result<T::IpsId, DispatchError> {
+        let bounded_metadata: BoundedVec<u8, T::MaxIpsMetadata> = metadata
+            .try_into()
+            .map_err(|_| Error::<T>::MaxMetadataExceeded)?;
 
-            Self::deposit_event(Event::<T>::IpsCreated(
-                sender,
-                name,
-                description,
-                metadata,
-                quantity,
-            ));
+        let ips_id = NextIpsId::<T>::try_mutate(|id| -> Result<T::IpsId, DispatchError> {
+            let current_id = *id;
+            *id = id
+                .checked_add(&One::one())
+                .ok_or(Error::<T>::NoAvailableIpsId)?;
+            Ok(current_id)
+        })?;
+
+        let info = IpsInfo {
+            metadata: bounded_metadata,
+            total_issuance: Default::default(),
+            owner: owner.clone(),
+            data,
+        };
+        IpsStorage::<T>::insert(ips_id, info);
+
+        Ok(ips_id)
+    }
+
+    /// Change the owner of an IP (Intellectual Property) Set (IPS)
+    pub fn change_owner(from: &T::AccountId, to: &T::AccountId, ipt: (T::IpsId, T::IptId)) -> DispatchResult {
+        IptStorage::<T>::try_mutate(ipt.0, ipt.1, |ipt_info| -> DispatchResult {
+            let mut info = ipt_info.as_mut().ok_or(Error::<T>::IptNotFound)?;
+            ensure!(info.owner == *from, Error::<T>::NoPermission);
+            if from == to {
+                return Ok(());
+            }
+
+            info.owner = to.clone();
+
+            IptByOwner::<T>::remove((from, ipt.0, ipt.1));
+            IptByOwner::<T>::insert((to, ipt.0, ipt.1), ());
 
             Ok(())
-        }
+        })
     }
+
+
 }
