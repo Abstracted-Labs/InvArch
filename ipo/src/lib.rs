fd65528056c17593bfdc3bd0b1e5abe83bebc156
Kresna Sucandra <kresnasucandra@gmail.com>
2021-09-26 08:36:41 +0800

    Fix: remove unused AccountData

--- ipo/src/lib.rs
+++ ipo/src/lib.rs
@@ -174,33 +174,6 @@ pub mod pallet {
         }
     }
 
-    /// All balance information for an account.
-    #[derive(
-        Encode, Decode, Clone, PartialEq, Eq, Default, RuntimeDebug, MaxEncodedLen, TypeInfo,
-    )]
-    pub struct AccountData<Balance> {
-        /// Non-reserved part of the balance. There may still be restrictions on this, but it is the
-        /// total pool what may in principle be transferred, reserved and used for tipping.
-        ///
-        /// This is the only balance that matters in terms of most operations on tokens. It
-        /// alone is used to determine the balance when in the contract execution environment.
-        pub free: Balance,
-        /// Balance which is reserved and may not be used at all.
-        ///
-        /// This can still get slashed, but gets slashed last of all.
-        ///
-        /// This balance is a 'reserve' balance that other subsystems use in order to set aside tokens
-        /// that are still 'owned' by the account holder, but which are suspendable.
-        /// This includes named reserve and unnamed reserve.
-        pub reserved: Balance,
-        /// The amount that `free` may not drop below when withdrawing for *anything except transaction
-        /// fee payment*.
-        pub misc_frozen: Balance,
-        /// The amount that `free` may not drop below when withdrawing specifically for transaction
-        /// fee payment.
-        pub fee_frozen: Balance,
-    }
-
     /// Errors for IPO pallet
     #[pallet::error]
     pub enum Error<T> {
