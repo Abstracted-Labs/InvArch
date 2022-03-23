315bf2d7ac7d916dc3b140e784812d6655c152cc
S0raWasTaken <s0ra@duck.com>
2022-03-23 05:06:20 -0500

    feat: Create `mint_should_fail` test

--- INV4/pallet-ipt/src/tests.rs
+++ INV4/pallet-ipt/src/tests.rs
@@ -35,3 +35,57 @@ fn mint_should_work() {
         );
     });
 }
+
+#[test]
+fn mint_should_fail() {
+    ExtBuilder::default().build().execute_with(|| {
+        Ipt::create(ALICE, 0, vec![(ALICE, ExistentialDeposit::get())]);
+
+        assert_eq!(
+            IptStorage::<Runtime>::get(0),
+            Some(AssetDetails {
+                owner: ALICE,
+                supply: ExistentialDeposit::get(),
+                deposit: 0,
+            })
+        );
+
+        // Case 0: Unknown origin
+        assert_noop!(
+            Ipt::mint(Origin::none(), 0, 1000, ALICE),
+            DispatchError::BadOrigin
+        );
+
+        assert_ne!(
+            IptStorage::<Runtime>::get(0),
+            Some(AssetDetails {
+                owner: ALICE,
+                supply: ExistentialDeposit::get() + 1000,
+                deposit: 0,
+            })
+        );
+
+        // Case 1: Ipt Does not exist
+        assert_noop!(
+            Ipt::mint(Origin::signed(ALICE), 32, 1000, ALICE),
+            Error::<Runtime>::IptDoesntExist
+        );
+
+        assert_eq!(IptStorage::<Runtime>::get(32), None);
+
+        // Case 2: Caller has no permission
+        assert_noop!(
+            Ipt::mint(Origin::signed(BOB), 0, 1000, ALICE),
+            Error::<Runtime>::NoPermission,
+        );
+
+        assert_ne!(
+            IptStorage::<Runtime>::get(0),
+            Some(AssetDetails {
+                owner: ALICE,
+                supply: ExistentialDeposit::get() + 1000,
+                deposit: 0,
+            })
+        );
+    });
+}
