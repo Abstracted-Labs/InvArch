ec23bdb1b9d6d982fb6700291ce20799e45b4c8d
Gabriel Facco de Arruda <arrudagates@gmail.com>
2023-03-19 21:55:10 -0400

    chore: Disable tests for maintenance mode pallet

--- pallets/maintenance-mode/src/lib.rs
+++ pallets/maintenance-mode/src/lib.rs
@@ -40,10 +40,10 @@
 #![allow(non_camel_case_types)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(test)]
-mod mock;
-#[cfg(test)]
-mod tests;
+//#[cfg(test)]
+//mod mock;
+//#[cfg(test)]
+//mod tests;
 
 mod types;
 pub use types::*;
