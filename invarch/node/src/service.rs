d9c11f7052ef2761fd8b2f4fb39f0e82b593085a
Gabriel Facco de Arruda <arrudagates@gmail.com>
2023-11-08 21:42:28 -0300

    clippy fixes

--- invarch/node/src/service.rs
+++ invarch/node/src/service.rs
@@ -411,7 +411,7 @@ pub fn parachain_build_import_queue(
         _,
     >(cumulus_client_consensus_aura::ImportQueueParams {
         block_import,
-        client: client.clone(),
+        client,
         create_inherent_data_providers: move |_, _| async move {
             let time = sp_timestamp::InherentDataProvider::from_system_time();
 
