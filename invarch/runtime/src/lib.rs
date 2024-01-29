0e7119eaaf49ad2b5b5759e1112bf2e7623f318b
Gabriel Facco de Arruda <arrudagates@gmail.com>
2024-01-29 13:16:49 +0800

    chore: Bump InvArch spec version

--- invarch/runtime/src/lib.rs
+++ invarch/runtime/src/lib.rs
@@ -139,7 +139,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("invarch"),
     impl_name: create_runtime_str!("invarch"),
     authoring_version: 1,
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
