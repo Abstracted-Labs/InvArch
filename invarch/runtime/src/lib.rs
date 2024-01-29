c9820d9daefaf236332f5b2aaf447610892703da
Gabriel Facco de Arruda <arrudagates@gmail.com>
2024-01-29 17:44:29 +0800

    fix: Time constants

--- invarch/runtime/src/lib.rs
+++ invarch/runtime/src/lib.rs
@@ -154,12 +154,14 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 /// Change this to adjust the block time.
 pub const MILLISECS_PER_BLOCK: u64 = 6000;
 
+pub const TWELVE_SECONDS_IN_MILLISECS: u64 = 12000;
+
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
 // Time is measured by number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
+pub const MINUTES: BlockNumber = 60_000 / (TWELVE_SECONDS_IN_MILLISECS as BlockNumber);
 pub const HOURS: BlockNumber = MINUTES * 60;
 pub const DAYS: BlockNumber = HOURS * 24;
 
