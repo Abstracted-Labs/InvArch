2c12f8094e1c089e6f12491c8605d9eb629dff05
Francisco Valentim Castilho <franciscoannyon@gmail.com>
2024-10-07 14:47:12 -0700

    Feat: upgrade sdk dependencies to stable2407 + Upgraded node using the stable2407 template + Added orml dependencies required for xcm

--- invarch/node/src/main.rs
+++ invarch/node/src/main.rs
@@ -1,14 +1,13 @@
-//! InvArch Parachain Node CLI.
+//! Substrate Parachain Node Template CLI
 
 #![warn(missing_docs)]
 
 mod chain_spec;
-#[macro_use]
-mod service;
 mod cli;
 mod command;
 mod rpc;
+mod service;
 
 fn main() -> sc_cli::Result<()> {
-    command::run()
+	command::run()
 }
--- new-modified-construct-runtime/src/construct_runtime/expand/hold_reason.rs
+++ new-modified-construct-runtime/src/construct_runtime/expand/hold_reason.rs
@@ -21,49 +21,55 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 pub fn expand_outer_hold_reason(pallet_decls: &[Pallet], scrate: &TokenStream) -> TokenStream {
-    let mut conversion_fns = Vec::new();
-    let mut hold_reason_variants = Vec::new();
-    for decl in pallet_decls {
-        if let Some(_) = decl.find_part("HoldReason") {
-            let variant_name = &decl.name;
-            let path = &decl.path;
-            let index = decl.index;
-            let instance = decl.instance.as_ref();
+	let mut conversion_fns = Vec::new();
+	let mut hold_reason_variants = Vec::new();
+	let mut hold_reason_variants_count = Vec::new();
+	for decl in pallet_decls {
+		if let Some(_) = decl.find_part("HoldReason") {
+			let variant_name = &decl.name;
+			let path = &decl.path;
+			let index = decl.index;
+			let instance = decl.instance.as_ref();
 
-            conversion_fns.push(composite_helper::expand_conversion_fn(
-                "HoldReason",
-                path,
-                instance,
-                variant_name,
-            ));
+			conversion_fns.push(composite_helper::expand_conversion_fn(
+				"HoldReason",
+				path,
+				instance,
+				variant_name,
+			));
 
-            hold_reason_variants.push(composite_helper::expand_variant(
-                "HoldReason",
-                index,
-                path,
-                instance,
-                variant_name,
-            ));
-        }
-    }
-    let hold_reason_variants_count = hold_reason_variants.len() as u32;
+			hold_reason_variants.push(composite_helper::expand_variant(
+				"HoldReason",
+				index,
+				path,
+				instance,
+				variant_name,
+			));
 
-    quote! {
-        /// A reason for placing a hold on funds.
-        #[derive(
-            Copy, Clone, Eq, PartialEq,
-            #scrate::__private::codec::Encode, #scrate::__private::codec::Decode, #scrate::__private::codec::MaxEncodedLen,
-            #scrate::__private::scale_info::TypeInfo,
-            #scrate::__private::RuntimeDebug,
-        )]
-        pub enum RuntimeHoldReason {
-            #( #hold_reason_variants )*
-        }
+			hold_reason_variants_count.push(composite_helper::expand_variant_count(
+				"HoldReason",
+				path,
+				instance,
+			));
+		}
+	}
 
-        impl #scrate::traits::VariantCount for RuntimeHoldReason {
-            const VARIANT_COUNT: u32 = #hold_reason_variants_count;
-        }
+	quote! {
+		/// A reason for placing a hold on funds.
+		#[derive(
+			Copy, Clone, Eq, PartialEq,
+			#scrate::__private::codec::Encode, #scrate::__private::codec::Decode, #scrate::__private::codec::MaxEncodedLen,
+			#scrate::__private::scale_info::TypeInfo,
+			#scrate::__private::RuntimeDebug,
+		)]
+		pub enum RuntimeHoldReason {
+			#( #hold_reason_variants )*
+		}
 
-        #( #conversion_fns )*
-    }
+		impl #scrate::traits::VariantCount for RuntimeHoldReason {
+			const VARIANT_COUNT: u32 = 0 #( + #hold_reason_variants_count )*;
+		}
+
+		#( #conversion_fns )*
+	}
 }
